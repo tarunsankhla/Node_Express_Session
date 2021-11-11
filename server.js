b4a754f6e7034d6d123fdd42b33a4c24288bcb61
Tether <tarun.sankhla.prc@gmail.com>
2021-11-11 22:49:07 +0530

    added cookie parser

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ const bodyparser = require('body-parser');
 const  session = require('express-session');
 const redis = require('redis');
 const redisStore = require('connect-redis')(session);
+const cookieParser = require('cookie-parser');
 const client = redis.createClient();
 const app = express();
 const router = express.Router();
+ 
+var email = 'xyz@gmail.com';
+var password = '1234pqr';
 
+app.use(cookieParser());
 app.use(session({
-    secret:"tarun1234",
+    secret:"MySecretKeyIstarun1234",
     store: new redisStore({host: 'localhost',client:client,ttl:260,port:6379}),
     saveUninitialized:false,
-    resave:false
+    resave:false,
+    cookie:{maxAge:1000 * 60 * 60 * 24},
 }))
 
 app.use(bodyparser.json());
@@ -20,15 +26,21 @@ app.use(bodyparser.urlencoded({extended:true}))
 router.get('/',(req,res) => {
     sess = req.session;
     if(sess.email){
-        return res.redirect('/admin');
+        return res.send("Welcome User <a href=\'/logout'>Click to Logout</a>");
     }
     res.sendFile('index.html');
 });
 
 router.post('/login',(req,res)=>{
+    if(req.body.email == email && req.body.password == password)
+    {
     sess = req.session;
     sess.email = req.body.email;
     res.end('done');
+    }
+    else{
+        res.send("Invalid credentials");
+    }
 });
 
 router.get('/admin',(req,res)=>{
